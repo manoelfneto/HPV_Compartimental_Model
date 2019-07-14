5a8eb23eec468d68b7f80eebac948db051457756
manoel <manoelbetmann@hotmail.com>
2019-07-13 22:36:28 -0300

    mudanca estado 1 pro 2 e 2 pro 0
    mudança de regras de estado q pro 2 e 2 pro 0, falta o 0 pro 1

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from tkinter import *
 import random
 import copy
 
+
 class Cell:
 
     def __init__(self, infect_time, state, age, gender):
-
         self.infect_time = infect_time
         self.state = state
         self.age = age
@@ -49,13 +49,13 @@ def make_frames():
 
 def put_cells():
     ages = []
-    aleatory_cells = [Cell(0, 0, 13, 0), Cell(1, 2, 22, 0), Cell(1, 1, 33,0 ), Cell(0, 0, 45, 0), Cell(0, 0, 25, 0),
-                 Cell(0, 0, 13, 1), Cell(0, 0, 13, 1), Cell(0, 0, 13, 1), Cell(0, 0, 13, 1), Cell(0, 0, 13, 1)]
+    aleatory_cells = [Cell(0, 0, 13, 0), Cell(1, 2, 22, 0), Cell(1, 1, 33, 0), Cell(0, 0, 45, 0), Cell(0, 0, 25, 0),
+                      Cell(0, 0, 13, 1), Cell(0, 0, 13, 1), Cell(0, 0, 13, 1), Cell(0, 0, 13, 1), Cell(0, 0, 13, 1)]
     for y in range(-1, 61):
         for x in range(-1, 81):
             firstGen[x][y] = random.choice(aleatory_cells)
             temporary[x][y] = 0
-            cell[x][y] = canvas.create_oval((x*10, y*10, x*10+10, y*10+10), outline="gray50", fill="black")
+            cell[x][y] = canvas.create_oval((x * 10, y * 10, x * 10 + 10, y * 10 + 10), outline="gray50", fill="black")
 
 
 def processing():
@@ -66,21 +66,23 @@ def processing():
 
     for y in range(0, 60):
         for x in range(0, 80):
-            infected_neighbors = search_neigh(x, y)  # vizinhos contaminados
+            infected_neighbors_state1 = search_neigh_state1(x, y)  # vizinhos contaminados
+            infected_neighbors_state2 = search_neigh_state2(x, y)  # vizingos apresentando sintomas
+            sum_infect_neigh = infected_neighbors_state1 + infected_neighbors_state2
 
-            if firstGen[x][y].getinfect_time() > 18:
+            if firstGen[x][y].getinfect_time() > 12:
                 temporary[x][y] = Cell(0, 0, 16, 0)
 
             else:
                 if firstGen[x][y].getstate() == 0:
-                    if infected_neighbors > 3:
+                    if sum_infect_neigh > 3:
                         temporary[x][y] = copy.copy(firstGen[x][y])
                         temporary[x][y].setstate(1)
                     else:
                         temporary[x][y] = copy.copy(firstGen[x][y])
 
                 if firstGen[x][y].getstate() == 1:
-                    if firstGen[x][y].getinfect_time() > 6 and firstGen[x][y].getinfect_time() < 12:
+                    if firstGen[x][y].getinfect_time() > 5 and firstGen[x][y].getinfect_time() < 8:
                         temporary[x][y] = copy.copy(firstGen[x][y])
                         temporary[x][y].setstate(2)
                         temporary[x][y].setinfect_time(firstGen[x][y].getinfect_time() + 1)
@@ -88,13 +90,12 @@ def processing():
                         temporary[x][y] = copy.copy(firstGen[x][y])
                         temporary[x][y].setinfect_time(temporary[x][y].getinfect_time() + 1)
 
-
                 if firstGen[x][y].getstate() == 2:
-                    if firstGen[x][y].getinfect_time() > 12:
+                    if firstGen[x][y].getinfect_time() > 8:
                         temporary[x][y] = copy.copy(firstGen[x][y])
                         temporary[x][y].setstate(1)
                         temporary[x][y].setinfect_time(temporary[x][y].getinfect_time() + 1)
-                        #print(temporary[x][y].getinfect_time())
+                        # print(temporary[x][y].getinfect_time())
                     else:
                         temporary[x][y] = copy.copy(firstGen[x][y])
                         temporary[x][y].setinfect_time(temporary[x][y].getinfect_time() + 1)
@@ -119,25 +120,64 @@ def processing():
             firstGen[x][y] = temporary[x][y]
 
 
-def search_neigh(a, b):  #vizinhos contaminados
-    infectted_neigh = 0
-    if firstGen[a - 1][b + 1].getstate() == 1 or firstGen[a - 1][b + 1].getstate() == 2:
-        infectted_neigh = infectted_neigh + 1
-    if firstGen[a][b + 1].getstate() == 1 or firstGen[a][b + 1].getstate() == 2:
-        infectted_neigh = infectted_neigh + 1
-    if firstGen[a + 1][b + 1].getstate() == 1 or firstGen[a + 1][b + 1].getstate() == 2:
-        infectted_neigh = infectted_neigh + 1
-    if firstGen[a - 1][b].getstate() == 1 or firstGen[a - 1][b].getstate() == 2:
-        infectted_neigh = infectted_neigh + 1
-    if firstGen[a + 1][b].getstate() == 1 or firstGen[a + 1][b].getstate() == 2:
-        infectted_neigh = infectted_neigh + 1
-    if firstGen[a - 1][b - 1].getstate() == 1 or firstGen[a - 1][b - 1].getstate() == 2:
-        infectted_neigh = infectted_neigh + 1
-    if firstGen[a][b - 1].getstate() == 1 or firstGen[a][b - 1].getstate() == 2:
-        infectted_neigh = infectted_neigh + 1
-    if firstGen[a + 1][b - 1].getstate() == 1 or firstGen[a + 1][b - 1].getstate() == 2:
-        infectted_neigh = infectted_neigh + 1
-    return infectted_neigh
+def search_neigh_state1(a, b):  # vizinhos contaminados
+    neigh_state1 = 0
+
+    if firstGen[a - 1][b + 1].getstate() == 1:
+        neigh_state1 += 1
+
+    if firstGen[a][b + 1].getstate() == 1:
+        neigh_state1 = neigh_state1 + 1
+
+    if firstGen[a + 1][b + 1].getstate() == 1:
+        neigh_state1 = neigh_state1 + 1
+
+    if firstGen[a - 1][b].getstate() == 1:
+        neigh_state1 = neigh_state1 + 1
+
+    if firstGen[a + 1][b].getstate() == 1:
+        neigh_state1 = neigh_state1 + 1
+
+    if firstGen[a - 1][b - 1].getstate() == 1:
+        neigh_state1 = neigh_state1 + 1
+
+    if firstGen[a][b - 1].getstate() == 1:
+        neigh_state1 = neigh_state1 + 1
+
+    if firstGen[a + 1][b - 1].getstate() == 1:
+        neigh_state1 = neigh_state1 + 1
+
+    return neigh_state1
+
+
+def search_neigh_state2(a, b):
+    neigh_state2 = 0
+
+    if firstGen[a - 1][b + 1].getstate() == 2:
+        neigh_state2 += 1
+
+    if firstGen[a][b + 1].getstate() == 2:
+        neigh_state2 += 1
+
+    if firstGen[a + 1][b + 1].getstate() == 2:
+        neigh_state2 += 1
+
+    if firstGen[a - 1][b].getstate() == 2:
+        neigh_state2 += 1
+
+    if firstGen[a + 1][b].getstate() == 2:
+        neigh_state2 += 1
+
+    if firstGen[a - 1][b - 1].getstate() == 2:
+        neigh_state2 += 1
+
+    if firstGen[a][b - 1].getstate() == 2:
+        neigh_state2 += 1
+
+    if firstGen[a + 1][b - 1].getstate() == 2:
+        neigh_state2 += 1
+
+    return neigh_state2
 
 
 def paint_cells():
