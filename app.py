1c6d629eb9e3b8349cfa96425028fdbccd128a57
manoel <manoelbetmann@hotmail.com>
2019-07-17 20:33:49 -0300

    mudança de nomes em variaveis

--- app.py
+++ app.py
@@ -62,24 +62,26 @@ def put_cells():
 
 
 def processing():
-    n_state0 = 0
-    n_state1 = 0
-    n_state2 = 0
+    cells_state_0 = 0
+    cells_state_1 = 0
+    cells_state_2 = 0
 
     for y in range(0, 60):
         for x in range(0, 80):
             infected_neighbors_state1 = search_neigh_state1(x, y)
             infected_neighbors_state2 = search_neigh_state2(x, y)
-            aleatory_cells = [Cell(0, 0, 13, 0), Cell(1, 2, 22, 0), Cell(1, 1, 33, 0), Cell(0, 0, 45, 0),
-                              Cell(0, 0, 25, 0), Cell(0, 0, 13, 1), Cell(0, 0, 13, 1), Cell(0, 0, 13, 1),
-                              Cell(0, 0, 13, 1), Cell(0, 0, 13, 1)]
+            aleatory_cells = [Cell(1, 2, 15, 0), Cell(0, 0, 22, 0), Cell(1, 1, 52, 0), Cell(0, 0, 59, 0),
+                              Cell(0, 0, 26, 0), Cell(0, 1, 17, 1), Cell(0, 0, 19, 1), Cell(0, 0, 57, 1),
+                              Cell(0, 0, 61, 1), Cell(0, 0, 28, 1), Cell(0, 0, 33, 0), Cell(1, 2, 45, 0),
+                              Cell(1, 1, 29, 0), Cell(0, 0, 45, 0), Cell(0, 0, 25, 0), Cell(0, 0, 35, 1),
+                              Cell(0, 0, 43, 1), Cell(0, 0, 36, 1), Cell(0, 0, 13, 1), Cell(0, 0, 24, 1)]
 
             if firstGen[x][y].getinfect_time() > 12:
                 temporary[x][y] = random.choice(aleatory_cells)
 
             else:
                 if firstGen[x][y].getstate() == 0:
-                    n_state0 += 1
+                    cells_state_0 += 1
                     if (infected_neighbors_state1 > 5 or infected_neighbors_state2 > 5) and \
                             (16 < firstGen[x][y].getage() > 25):
                         temporary[x][y] = copy.copy(firstGen[x][y])
@@ -91,7 +93,7 @@ def processing():
                         temporary[x][y] = copy.copy(firstGen[x][y])
 
                 elif firstGen[x][y].getstate() == 1:
-                    n_state1 += 1
+                    cells_state_1 += 1
                     if firstGen[x][y].getinfect_time() == 5:
                         temporary[x][y] = copy.copy(firstGen[x][y])
                         temporary[x][y].setstate(2)
@@ -101,7 +103,7 @@ def processing():
                         temporary[x][y].setinfect_time(temporary[x][y].getinfect_time() + 1)
 
                 elif firstGen[x][y].getstate() == 2:
-                    n_state2 += 1
+                    cells_state_2 += 1
                     if firstGen[x][y].getinfect_time() > 8:
                         temporary[x][y] = copy.copy(firstGen[x][y])
                         temporary[x][y].setstate(1)
@@ -115,9 +117,9 @@ def processing():
     content = archive2.readlines()
     number_of_lines = len(content)
     archive.write("round: %d" % ((number_of_lines / 5) + 1) + "\n")
-    archive.write("cells state 0: %d" % n_state0 + "\n")
-    archive.write("cells state 1: %d" % n_state1 + "\n")
-    archive.write("cells state 2: %d" % n_state2 + "\n")
+    archive.write("cells state 0: %d" % cells_state_0 + "\n")
+    archive.write("cells state 1: %d" % cells_state_1 + "\n")
+    archive.write("cells state 2: %d" % cells_state_2 + "\n")
     archive.write("----------------------------" + "\n")
 
     for y in range(0, 60):
